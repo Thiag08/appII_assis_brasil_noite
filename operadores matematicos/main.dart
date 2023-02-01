14e75c4c32d0f3949d309d78a29d9a3590b9d7d6
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-01-31 22:07:40 -0300

    Operadores matemáticos

---
+++ operadores matematicos/main.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // Entrada
+  var celsius = 30;
+
+  // Processamento
+  var fahrenheit = celsius * 1.8 + 32;
+  var kelvin = celsius + 273.15;
+
+  // Saída
+  print('celsiusºC é igual à fahrenheitºF');
+}
